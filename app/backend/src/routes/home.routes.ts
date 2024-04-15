f8ed9ced34d45fe408ef1f298cfa0380b537580f
Leonardo Martins <leonardogustavo@outlook.com>
2024-04-15 19:27:29 -0300

    Comment

--- app/backend/src/routes/home.routes.ts
+++ app/backend/src/routes/home.routes.ts
@@ -1,13 +1,20 @@
 import { Request, Router, Response } from 'express';
-import csvtojsonV2 from "csvtojson";
+import csvtojsonV2 from 'csvtojson';
 
 const router = Router();
 
 function csvToJson(): any | void {
-    const csvFilePath = '../../atualizacao_preco_exemplo.csv';
-    const data = csvtojsonV2().fromFile(csvFilePath).then((jsonObj) => jsonObj);
-    return data;
+  const csvFilePath = '../../atualizacao_preco_exemplo.csv';
+  const data = csvtojsonV2({ checkType: true })
+    .fromFile(csvFilePath)
+    .then((jsonObj) => jsonObj);
+  return data;
 }
+
+
+
+// IDEIA -> qdo for comparar com o banco, baixa tudo do banco de uma vez e comparada com o que baixar. Não baixa toda vez.
+
 
 router.post('/', csvToJson(), (req: Request, res: Response) => {});
 
