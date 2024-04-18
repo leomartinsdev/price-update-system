16f40780cb2ce91252fd3e9db2f1c99ac05476af
Leonardo Martins <leonardogustavo@outlook.com>
2024-04-18 20:26:00 -0300

    Instalei o react-router-dom. Preparei o App.tsx e o main.tsx. Criei a página principal

---
+++ app/frontend/src/pages/UpdatePrices.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, ReactElement } from 'react';
+
+const UpdatePrices = (): ReactElement => {
+    return (
+        <>
+            <h1>
+                <p>
+                    FUNCIONOU
+                </p>
+            </h1>
+        </>
+    );
+}
+
+export default UpdatePrices;
