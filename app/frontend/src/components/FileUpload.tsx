ff4fb8228f57e182e71d5b4cdea5c19cd00d0919
Leonardo Martins <leonardogustavo@outlook.com>
2024-04-19 00:13:46 -0300

    Projeto 100% funcional - check

--- app/frontend/src/components/FileUpload.tsx
+++ app/frontend/src/components/FileUpload.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect, useRef, useState } from 'react';
+import React, { ReactElement, useRef, useState } from 'react';
 
 interface IProductDTO {
   code: number | string;
@@ -10,11 +10,11 @@ interface IProductDTO {
 
 const FileUpload = (): ReactElement => {
   const [products, setProducts] = useState<IProductDTO[]>([]);
-  const [isValid, setIsValid] = useState<boolean>(false);
+  const [isDisabled, setIsDisabled] = useState<boolean>(true);
 
   const fileInput = useRef<HTMLInputElement>(null);
 
-  const handleSubmit = async (event: React.FormEvent) => {
+  const handleValidateSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
     if (!fileInput.current || !fileInput.current.files) return;
@@ -28,40 +28,56 @@ const FileUpload = (): ReactElement => {
       body: formData,
     });
 
-    setProducts((await response.json()).validatedProducts);
-
-    // checkIfItsValid();
-
     if (response.ok) {
       console.log('File uploaded successfully');
+      const data = (await response.json()).validatedProducts;
+      setProducts(data);
+
+      const allValid = data.every(
+        (product: { validation: string }) => product.validation === 'OK'
+      );
+      setIsDisabled(!allValid); // allValid retorna true se todos os produtos são validos. Portanto preciso passar false para que disabled=false.
     } else {
       console.log('File upload failed');
     }
   };
-  
-  useEffect(() => {
-    setIsValid(!!products && products.every((product) => product.validation.toUpperCase() === 'OK'));
-  }, [products])
-  
-  console.log(isValid);
+
+  const handleUpdateSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const update = await fetch('http://localhost:3001/products', {
+      method: 'PATCH',
+    });
+
+    if (update.ok) {
+      console.log('Products updated successfully');
+      setProducts([]);
+      fileInput.current.value = '';
+      setIsDisabled(true);
+    } else {
+      console.log('Products update failed');
+    }
+  };
+
+  console.log(isDisabled);
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleValidateSubmit}>
         <label htmlFor="file">Carregue o arquivo CSV:</label>
         <input type="file" id="file" name="file" ref={fileInput} />
         <button type="submit">Validar</button>
       </form>
 
-      <button disabled={!isValid || !!products}>Atualizar</button>
+      <button type="button" disabled={isDisabled} onClick={handleUpdateSubmit}>Atualizar</button>
 
       {products &&
         products.map((product) => (
           <div key={product.code}>
             <p>Código: {product.code}</p>
-            <p>Produto: {product.name}</p>
-            <p>Preço atual: {product.current_price}</p>
-            <p>Novo preço: {product.new_price}</p>
-            <p>Válido: {product.validation}</p>
+            <p>Nome: {product.name}</p>
+            <p>Preço Atual: {product.current_price}</p>
+            <p>Novo Preço: {product.new_price}</p>
+            <p>Validação: {product.validation}</p>
           </div>
         ))}
     </>
